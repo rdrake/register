89538d9a754567d22f1b306c5194c2eecc8a12c3
Richard Drake <rdrake@rdrake.org>
2013-01-11 20:26:52 -0500

    sdfs

--- forms.py
+++ forms.py
@@ -17,20 +17,20 @@ class RegistrationForm(Form):
 	])
 	confirm = PasswordField("Confirm Password")
 
-	first_name = TextField("First Name", validators=[Required()], default="Richard")
-	last_name = TextField("Last Name", validators=[Required()], default="Drake")
+	first_name = TextField("First Name", validators=[Required()])
+	last_name = TextField("Last Name", validators=[Required()])
 
 	apt = TextField("Apt")
-	street = TextField("Street", validators=[Required()], default="959 Renfrew Court")
-	city = TextField("City", validators=[Required()], default="Oshawa")
+	street = TextField("Street", validators=[Required()])
+	city = TextField("City", validators=[Required()])
 	#province = TextField("Province", validators=[Required()], default="ON")
-	postal_code = TextField("Postal Code", validators=[Required(), Regexp("^\w{1}\d{1}\w{1}\s{0,1}\d{1}\w{1}\d{1}$")], default="L1J 6L2")
+	postal_code = TextField("Postal Code", validators=[Required(), Regexp("^\w{1}\d{1}\w{1}\s{0,1}\d{1}\w{1}\d{1}$")])
 
-	primary_phone = TelField("Primary Phone", validators=[Required()], default="9054423152")
+	primary_phone = TelField("Primary Phone", validators=[Required()])
 	secondary_phone = TelField("Secondary Phone", validators=[Optional()])
 
 	marketing = BooleanField("I'd like to receive emails from N.A.S.C. Soccer, Softball, and their affiliates", default=True)
-	volunteering = BooleanField("I'd like to volunteer, please contact me with information on how to volunteer", default=False)
+	volunteering = BooleanField("I'd like to volunteer, please contact me with information on how to volunteer")
 
 PLAYED_YEARS = zip(map(str, range(20)), map(str, range(20)))
 GENDERS = [("M", "Male"), ("F", "Female")]
@@ -38,17 +38,14 @@ GENDERS = [("M", "Male"), ("F", "Female")]
 AGE_GROUPS = [(ag.id, ag.name) for ag in AgeGroup.query.all()]
 
 class PlayerRegistrationForm(Form):
-	first_name = TextField("First Name", validators=[Required()], default="Jonathan")
-	last_name = TextField("Last Name", validators=[Required()], default="Drake")
-
-	date_of_birth = TextField("Date of Birth", validators=[Required()], default="1999-02-01")
+	first_name = TextField("First Name", validators=[Required()])
+	last_name = TextField("Last Name", validators=[Required()])
+	date_of_birth = TextField("Date of Birth", validators=[Required()])
 
 	gender = SelectField("Gender", choices=GENDERS)
 	age_group_id = SelectField("Program", choices=AGE_GROUPS, coerce=int)
-	#gender = TextField("Gender", validators=[Required()], default="M")
 
 	played_before = BooleanField("Played Before")
-	#played_years = IntegerField("Number of Years", default=0)
 	played_years = SelectField("Number of Years", choices=PLAYED_YEARS)
 	played_position = TextField("Position")
 
